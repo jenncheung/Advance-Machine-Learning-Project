2f831e5f072438b6e9e2a98ece8dbbb294168e46
jenncheung <jenncheung2015+github@gmail.com>
2016-12-12 21:57:19 -0500

    Finished main part

--- main.m
+++ main.m
@@ -3,17 +3,22 @@ imset = imageSet('train', 'recursive');
 trainingPercents = [0.2 0.15 0.1 0.05 0.01];
 averageErrors = zeros(size(trainingPercents,1));
 
-for m=1:size(trainingPercents,1)
+for m=1:size(trainingPercents,2)
 	fprintf('Training with %2.2f%%\n', trainingPercents(m) * 100)
 	individualErrors = zeros([5 2]);
 	for n=1:5
 		[individualErrors(n,1), individualErrors(n,2)] = cnnWithLinearSVM(cnnModel, imset, trainingPercents(m));
 	end 
 	fprintf('Crossvalidation Accuracy: ')
-	individualError(:, 1)
+	individualErrors(:, 1)
 	fprintf('Test Set Accuracy: ')
-	individualError(:, 2)
+	individualErrors(:, 2)
 	fprintf('Average Crossvalidation and Test Set Accuracy: ')
-	mean(individualError(:, 1))
-	averageErrors(m) = mean(individualError(:, 2))
+	mean(individualErrors(:, 1))
+	averageErrors(m) = mean(individualErrors(:, 2))
 end
+
+plot(trainingPercents,averageErrors);
+title('Error with percent training set');
+xlabel('Percent Training');
+ylabel('Percent accuracy with Test Set');
